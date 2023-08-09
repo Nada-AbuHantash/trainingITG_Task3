e6a41089f2558e89eb210ebc07f94be245dd9718
nada mahmoud <nada.m.abuhantash@gmail.com>
2023-08-09 22:44:38 +0300

    add image when add product

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const _ = require('lodash');
 const { Product, validate } = require('../models/products');
 const { Image } = require('../models/images');
-const admin = require('../middleware/admin');
+const upload = require('../middleware/upload');
+const validationimage = require('../middleware/validation');
 const express = require('express');
-const auth = require('../middleware/auth');
+const path = require('path');
 const router = express.Router();
 
 let message = " ";
@@ -11,12 +12,25 @@ let message = " ";
 router.get('/add', async (req, res, resp) => {
     res.render('addproduct', { message });
 });
-router.post('/add', async (req, res) => {
+router.post('/add', [upload.single('file'), validationimage], async (req, res) => {
 
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    const { filename, mimetype, size } = req.file;
+    uniqueName = filename;
 
-    const image = await Image.findById(req.body.imageId);
+    const filePath = path.join('C:\\Users\\anas1\\vscode-nodejs\\Task3\\image', filename);
+
+    let image = new Image({
+
+        originalname: req.file.originalname,
+        uniqueName: uniqueName,
+        filePath: filePath,
+        mimetype: mimetype,
+        size: size
+    });
+    await image.save();
+    const imageId = image._id.toString(16);
+
+    image = await Image.findById(imageId);
     if (!image) return res.status(400).send('Invalid image.');
 
 
@@ -27,7 +41,7 @@ router.post('/add', async (req, res) => {
             longDescribe: req.body.longDescribe,
             category: req.body.category,
             price: req.body.price,
-            image: req.body.imageId,
+            image: imageId,
         }
     );
     await product.save();
@@ -42,13 +56,10 @@ router.get('/', async function (req, res, next) {
 
     const allimage = await Image
         .find({ _id: { $in: allProduct.map((product) => product.image) } });
-    //  const filePaths = allimage.map((image) => image.filePath);
-    //  console.log(filePaths);
-    //  console.log(  _.pick(allimage, ['uniqueName', 'filePath']));
-    // console.log(allimage.filePath);
+
     const userId = req.session.userId;
     message = req.flash('success');
-    const isAdmin=req.session.isAdmin;
-    res.render('home', { allProduct, allimage, userId, message ,isAdmin});
+    const isAdmin = req.session.isAdmin;
+    res.render('home', { allProduct, allimage, userId, message, isAdmin });
 });
 module.exports = router;
